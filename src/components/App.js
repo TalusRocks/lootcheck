78bae2b0964ac631cddbdce9c3aef4e6f44c7cd7
Jensen <cwuj@M-C02T12W8GTDX.nordstrom.net>
2018-05-29 14:29:03 -0700

    convert app to stateless component, ignore src/index in coverage test

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Wallet from './Wallet'
 import Loot from './Loot'
 
-class App extends Component {
-    render() {
-        return (
-            <div className="wrapper">
-                <h1>LOOT CHECK</h1>
-                <Loot/>
-                <Wallet/>
-             </div>
-        )
-    }
+const App = () => {
+    return (
+        <div className="wrapper">
+            <h1>LOOT CHECK</h1>
+            <Loot/>
+            <Wallet/>
+            </div>
+    )
 }
 
 export default App
